6ff74bc946b64765c3a3dd474d19967dd5412967
tsavpyn <tsavpyn@outlook.com>
2024-06-16 23:22:06 +0800

    init

---
+++ astro.config.ts
@@ -0,0 +1,35 @@
+import {defineConfig, passthroughImageService} from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+    image: {
+        service: passthroughImageService()
+    },
+    integrations: [
+        starlight({
+            title: 'Lyricify',
+            logo: {
+                src: '/src/assets/icon.webp',
+            },
+            social: {
+                github: 'https://github.com/WXRIW/Lyricify-App',
+            },
+            sidebar: [
+                {
+                    label: '快速上手',
+                    autogenerate: {directory: 'get-started'}
+                },
+                {
+                    label: '常见问题',
+                    autogenerate: {directory: 'faq'},
+                },
+            ],
+            customCss: [
+                // 你的自定义 CSS 文件的相对路径
+                './src/styles/custom.css',
+                './src/fonts/fontface.css',
+            ],
+        }),
+    ],
+});
