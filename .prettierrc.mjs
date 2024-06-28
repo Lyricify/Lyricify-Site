e13738016edd6a81fdf585671e78a552c70bcb5a
Sheng Fan <fredtools999@gmail.com>
2024-06-28 23:17:55 +0800

    chore: reformat project

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,12 +1,16 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
-};
+    plugins: ['prettier-plugin-astro'],
+    overrides: [
+        {
+            files: '*.astro',
+            options: {
+                parser: 'astro',
+            },
+        },
+    ],
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+}
