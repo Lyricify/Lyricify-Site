e5ee689e7a6883153b101164e1ce13743a549036
tsavpyn <tsavpyn@outlook.com>
2024-06-29 02:49:53 +0800

    Reformatted code Update `prettier` config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,12 +2,27 @@
 export default {
     plugins: ['prettier-plugin-astro'],
     bracketSpacing: false,
-    embbedLanguageFormatting: 'off',
+    embedLanguageFormatting: 'off',
+    editorconfig: true,
     jsxSingleQuote: true,
     trailingComma: 'es5',
     singleQuote: true,
     tabWidth: 4,
     semi: true,
     printWidth: 120,
-    proseWrap: false,
+    "overrides": [
+        {
+            "files": "*.json",
+            "options": {
+                "tabWidth": 2,
+                singleQuote: false,
+            }
+        },
+        {
+            "files": ["*.html", "legacy/**/*.js"],
+            "options": {
+                "tabWidth": 4
+            }
+        }
+    ]
 };
