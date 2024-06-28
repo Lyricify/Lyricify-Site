4a19758db232c7480beb1a0e12abf35f3bd57801
tsavpyn <tsavpyn@outlook.com>
2024-06-28 19:25:39 +0800

    Hot fix to `v1.0 Patch 1` + Fix the language switcher + Removed unused funcs in DataHelper.ts

--- src/data/DataHelper.ts
+++ src/data/DataHelper.ts
@@ -1,17 +1,5 @@
 import data from '@data/Data.json';
-import labels from '@data/Labels.json'
+import labels from '@data/Labels.json';
 
 export const Data: object = data;
 export const Labels: object = labels;
-
-export function useArgs(origin: string, variables: Record<string, string>): string {
-    let output: string = origin;
-    Object.entries(variables).map(([key, item]) => {
-        output = output.replace(new RegExp(`\\[${key}]`), item);
-    })
-    return output;
-}
-
-export const t = (translatable: object, locale: string) => {
-    return translatable[locale]
-}
